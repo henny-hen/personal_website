0ecdf18a208be17748d37d45d73681fe89f2acb4
HENNY Sánchez <80646003+henny-hen@users.noreply.github.com>
2026-06-10 15:26:58 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,18 @@ const workItems: Item[] = [
   {
     title: "Axon Networks",
     role: "Software Engineer Intern",
-    period: "sep 2024 - may 2025",
+    period: "jan 2025 - may 2025",
     description:
       "Optimizing and maintaining the company's internal platforms and products' algorithms, focusing on performance improvements, new features, and scalability.",
     href: "https://www.axon-networks.com/",
+  },
+  {
+    title: "Zaleos",
+    role: "Software Engineer Intern",
+    period: "sep 2025 - mar 2026",
+    description:
+      "Delivering features in Java and Golang under the NGCS Call Location and Services team on a Linux-based environment.",
+    href: "https://zaleos.net/es/",
   },
 ]
 
@@ -19,13 +27,13 @@ const projectItems = [
     title: "DASOS UPM",
     role: "creator and maintainer",
     description:
-      "open-source project for the extraction and analysis of data from the UPM's academic system",
+      "open-source project for the extraction and analysis of data from the UPM's academic system, using statistic advanced metrics.",
     href: "https://dasos-upm-e2ck.vercel.app/",
   },
   {
     title: "Planificator",
     role: "creator",
-    description: "Android calendar application with mood-based event registry for daily activity tracking",
+    description: "Android calendar application with mood-based event registry for daily activity tracking.",
     href: "https://github.com/henny-hen/Planificator",
   },
 ]
